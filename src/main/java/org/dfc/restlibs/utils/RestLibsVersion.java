5ba46eb571a29c520e1b690515c99c8eaacf61dc
michael-conway <michael_conway@unc.edu>
2016-10-04 14:52:39 -0400

    #2 compiles

--- src/main/java/org/dfc/restlibs/utils/RestLibsVersion.java
+++ src/main/java/org/dfc/restlibs/utils/RestLibsVersion.java
@@ -1,5 +1,5 @@
 package org.dfc.restlibs.utils;
 public final class RestLibsVersion {
  public static String VERSION="1.0.0.0-SNAPSHOT";
- public static String BUILD_TIME="2016-09-27T20:18:36Z";
+ public static String BUILD_TIME="2016-10-04T18:52:26Z";
 }
