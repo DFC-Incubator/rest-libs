ef2f6e18b9144ae92e1c61c92001a2bb74b14d3a
michael-conway <michael_conway@unc.edu>
2016-10-17 14:37:16 -0400

    #2 first working

--- src/main/java/org/dfc/restlibs/utils/RestLibsVersion.java
+++ src/main/java/org/dfc/restlibs/utils/RestLibsVersion.java
@@ -1,5 +1,5 @@
 package org.dfc.restlibs.utils;
 public final class RestLibsVersion {
  public static String VERSION="1.0.0.0-SNAPSHOT";
- public static String BUILD_TIME="2016-10-13T14:42:29Z";
+ public static String BUILD_TIME="2016-10-13T14:46:28Z";
 }
--- src/main/java/org/irods/jargon/rest/exception/IrodsRestRuntimeException.java
+++ src/main/java/org/irods/jargon/rest/exception/IrodsRestRuntimeException.java
@@ -12,10 +12,25 @@ import org.irods.jargon.core.exception.JargonRuntimeException;
  *
  */
 public class IrodsRestRuntimeException extends JargonRuntimeException {
-
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -1507966468925924341L;
+
+	public IrodsRestRuntimeException() {
+		super();
+	}
+
+	public IrodsRestRuntimeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public IrodsRestRuntimeException(String message) {
+		super(message);
+	}
+
+	public IrodsRestRuntimeException(Throwable cause) {
+		super(cause);
+	}
 
 }
